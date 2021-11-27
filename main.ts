8c87432a1024ba149fca252324abbf1f6eb8ac9a
stprz <95131792+stprz@users.noreply.github.com>
2021-11-27 14:30:06 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,19 @@
+input.onButtonPressed(Button.A, function () {
+    item += 1
+})
+let item = 0
+weatherbit.startWeatherMonitoring()
+basic.forever(function () {
+    if (item == 0) {
+        basic.showString("Temp C: ")
+        basic.showNumber(Math.idiv(weatherbit.temperature(), 100))
+    } else if (item == 1) {
+        basic.showString("Humidity %: ")
+        basic.showNumber(Math.idiv(weatherbit.humidity(), 1024))
+    } else if (item == 2) {
+        basic.showString("Pressure hPa: ")
+        basic.showNumber(Math.idiv(weatherbit.pressure(), 25600))
+    } else {
+        item = 0
+    }
+})
